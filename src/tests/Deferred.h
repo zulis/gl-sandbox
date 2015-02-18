94bbc010f30c60298908945b876c70dec2599c98
Tadas Zulonas <tadas.zulonas@gmail.com>
2015-02-18 17:19:48 +0200

    resources

--- src/tests/Deferred.h
+++ src/tests/Deferred.h
@@ -7,12 +7,20 @@
 //#include "core/Transform3d.h"
 //#include "core/Texture.h"
 #include "core/Resource.h"
+#include <iostream>
 
-class Abc
+class Image : public Shared<Image>
 {
 public:
-    Abc(const std::string& fileName, int sk) : mFileName(fileName)
-	{}
+	Image(const std::string& fileName, int sk) : mFileName(fileName)
+	{
+		std::cout << "create:" << mFileName << std::endl;
+	}
+
+	~Image()
+	{
+		std::cout << "release:" << mFileName << std::endl;
+	}
 
 	std::string mFileName;
 };
@@ -43,10 +51,30 @@ private:
 //=========================================================================
 void Deferred::setup()
 {
-	auto a1 = Resource::create<Abc>("aaa.txt", 1);
-	auto a2 = Resource::create<Abc>("bbb.txt", 2);
-	auto a3 = Resource::create<Abc>("aaa.txt", 3);
+	{
+		auto a1 = Resource::create<Image>("aaa", 1);
+		auto a2 = Resource::create<Image>("aaa", 1);
+		auto a3 = Resource::create<Image>("aaa", 1);
+		std::cout << "--------------------" << std::endl;
+		//auto b1 = Image::create("bbb", 1);
+		//auto b2 = Image::create("bbb", 1);
+		//auto b3 = Image::create("bbb", 1);
+	}
+	
+	/*
+	std::cout << "--------------------" << std::endl;
 
+	auto b1 = std::make_unique<Abc>("bbb", 5);
+	auto b2 = std::make_unique<Abc>("bbb", 5);
+	auto b3 = std::make_unique<Abc>("bbb", 5);
+
+	(*b1).mFileName = "pakeistas";
+	std::cout << b2.get()->mFileName << std::endl;
+	std::cout << b3.get()->mFileName << std::endl;*/
+
+	int i;
+
+	i = 0;
 	/*mCamera = Camera::create();
 	mCamera->setRotateSpeed(0.002f);
 	mCamera->setStrafeSpeed(mStrafeSpeed);
@@ -66,36 +94,36 @@ void Deferred::input(Input& input)
 
 	/*if (input.isKeyDown(KEY_LEFT_SHIFT))
 		mCamera->setStrafeSpeed(mStrafeFastSpeed);
-	else
+		else
 		mCamera->setStrafeSpeed(mStrafeSpeed);
 
-	if (input.isKeyDown(KEY_W))
+		if (input.isKeyDown(KEY_W))
 		mCamera->move(Camera::FORWARD);
-	else if (input.isKeyDown(KEY_S))
+		else if (input.isKeyDown(KEY_S))
 		mCamera->move(Camera::BACKWARD);
 
-	if (input.isKeyDown(KEY_A))
+		if (input.isKeyDown(KEY_A))
 		mCamera->move(Camera::LEFT);
-	else if (input.isKeyDown(KEY_D))
+		else if (input.isKeyDown(KEY_D))
 		mCamera->move(Camera::RIGHT);
 
-	if (input.isKeyDown(KEY_E))
+		if (input.isKeyDown(KEY_E))
 		mCamera->move(Camera::UP);
-	else if (input.isKeyDown(KEY_Q))
+		else if (input.isKeyDown(KEY_Q))
 		mCamera->move(Camera::DOWN);
 
-	if (input.getMouseScroolY() != 0)
+		if (input.getMouseScroolY() != 0)
 		mCamera->move(input.getMouseScroolY() > 0 ? Camera::FORWARD : Camera::BACKWARD);
 
-	if (input.isMouseDown(MouseButton::Right))
-	{
+		if (input.isMouseDown(MouseButton::Right))
+		{
 		input.hideMouse();
 		mCamera->rotate((float)input.getMouseChangeX(), (float)input.getMouseChangeY());
-	}
-	else
-	{
+		}
+		else
+		{
 		input.showMouse();
-	}*/
+		}*/
 }
 
 //=========================================================================
