1e2d5500d47b4e68e4dd6d95fb0175e7730485be
Tadas Zulonas <tadas.zulonas@gmail.com>
2017-08-17 00:16:12 +0300

    libs update

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,18 +6,18 @@
 
 #include "tests/Test.h"
 #include "tests/Game.h"
-#include "tests/Deferred1.h"
-#include "tests/Deferred2.h"
-#include "tests/Deferred3.h"
-#include "tests/MeshTest.h"
+//#include "tests/Deferred1.h"
+//#include "tests/Deferred2.h"
+//#include "tests/Deferred3.h"
+//#include "tests/MeshTest.h"
 
 void main(int argc, char *argv[])
 {
 	//Test test;
-	//Game test;
+	Game test;
 	//Deferred1 test;
 	//Deferred2 test;
-	Deferred3 test;
+	//Deferred3 test;
 	//MeshTest test;
 	test.run();
 }
