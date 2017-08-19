a38ef6d4f27d829ab3693d32ba9492d6255071fc
Tadas Zulonas <tadas.zulonas@gmail.com>
2017-08-20 01:13:22 +0300

    .

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,8 +5,8 @@
 */
 
 #include "tests/Test.h"
-#include "tests/Game.h"
-//#include "tests/Deferred1.h"
+//#include "tests/Game.h"
+#include "tests/Deferred1.h"
 //#include "tests/Deferred2.h"
 //#include "tests/Deferred3.h"
 //#include "tests/MeshTest.h"
@@ -14,8 +14,8 @@
 void main(int argc, char *argv[])
 {
 	//Test test;
-	Game test;
-	//Deferred1 test;
+	//Game test;
+	Deferred1 test;
 	//Deferred2 test;
 	//Deferred3 test;
 	//MeshTest test;
