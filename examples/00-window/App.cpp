d23ea59d6a60c543c0aac207b41ae67ba9533dcf
tadas <tadas.zulonas@gmail.com>
2018-03-09 15:41:24 +0200

    work in progress

--- examples/00-window/App.cpp
+++ examples/00-window/App.cpp
@@ -1,7 +1,13 @@
 #include "App.h"
+#include <imgui.h>
+#include "filesystem/FileSystem.h"
+
+using namespace library;
+using fs = FileSystem;
 
 App::App() : BaseApp("Window")
 {
+    const char *data = fs::readFile("assets/textures/uv.jpg");
 }
 
 void App::update(double deltaTime)
@@ -14,6 +20,10 @@ void App::draw()
     glClearColor(0.39f, 0.58f, 0.93f, 1.f);
     // Clear color buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //ImGui::SetNextWindowSize(ImVec2(400, 300));
+    //ImGui::Begin("Hello!");
+    //ImGui::End();
 }
 
 void App::onResize(int width, int height)
---
+++ library/filesystem/FileSystem.h
@@ -0,0 +1,13 @@
+#ifndef PROJECT_FILESYSTEM_H
+#define PROJECT_FILESYSTEM_H
+
+namespace library
+{
+class FileSystem
+{
+public:
+    static const char *readFile(const char *fileName);
+};
+}
+
+#endif //PROJECT_FILESYSTEM_H
