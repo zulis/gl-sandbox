d9f812065880fe6adc18502886a0843d31e09423
Tadas Zulonas <tadas.zulonas@gmail.com>
2018-03-12 01:26:17 +0200

    work in progress

--- library/system/Log.h
+++ library/system/Log.h
@@ -1,10 +1,19 @@
 #ifndef PROJECT_LOG_H
 #define PROJECT_LOG_H
 
+#include <spdlog/spdlog.h>
+
 namespace library
 {
-#define note(fmt, ...) {printf(fmt, ##__VA_ARGS__);printf("\n");}
-#define error(fmt, ...) {printf(fmt, ##__VA_ARGS__);printf("\n");}
+
+static std::shared_ptr<spdlog::logger> logger = spdlog::stderr_logger_mt("console");
+//static std::shared_ptr<spdlog::logger> logger = spdlog::basic_logger_mt("Log", "log.txt");
+
+#define note(...) library::logger->info(__VA_ARGS__)
+#define error(...) library::logger->error(__VA_ARGS__)
+
+//#define note(fmt, ...) {printf(fmt, ##__VA_ARGS__);printf("\n");}
+//#define error(fmt, ...) {printf(fmt, ##__VA_ARGS__);printf("\n");}
 }
 
 #endif //PROJECT_LOG_H
